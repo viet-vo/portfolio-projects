7a4ecf359166065eacbbae6d320e679d53b51a38
Viet Vo <viet.allen.qvo@gmail.com>
2020-03-06 15:21:35 -0800

    Setup board and user input

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,1 +1,27 @@
-// intialize
+// Dependencies
+const inquirer = require("inquirer");
+
+const prompt = inquirer.createPromptModule();
+
+let game = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+const askForMove = () => {
+  prompt([
+    {
+      type: "list",
+      name: "choice1",
+      message: "Choose between: ",
+      choices: ["A", "B", "C"]
+    }
+  ]).then(choice => console.log(choice.choice1));
+};
+
+console.log(game[0]);
+console.log(game[1]);
+console.log(game[2]);
+
+askForMove();
