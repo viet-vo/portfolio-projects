35ed2d8684a7c7d302afe049204ef767fb9296a8
Viet Vo <viet.allen.qvo@gmail.com>
2020-03-07 16:54:37 -0800

    Finished game check. TODO needs to check for tie games and remove choices after they are chosen

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,20 +1,26 @@
 // Dependencies
 const inquirer = require("inquirer");
 
+// Creating inquirer pronpts
 const prompt = inquirer.createPromptModule();
 
+// Game and player variable
 let game = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ];
 
+let player = "x";
+
+// Recursive function that ask for player's choice until there is a winner
 const askForMove = () => {
   prompt([
     {
       type: "list",
       name: "choice1",
       message: "Choose between: ",
+      // TODO Need to remove choices after they are chosen
       choices: [
         "topLeft",
         "topMiddle",
@@ -30,38 +36,43 @@ const askForMove = () => {
   ]).then(choice => {
     switch (choice.choice1) {
       case "topLeft":
-        game[0][0] = "x";
+        game[0][0] = player;
         break;
       case "topMiddle":
-        game[0][1] = "x";
+        game[0][1] = player;
         break;
       case "topRight":
-        game[0][2] = "x";
+        game[0][2] = player;
         break;
       case "middleLeft":
-        game[1][0] = "x";
+        game[1][0] = player;
         break;
       case "middleMiddle":
-        game[1][1] = "x";
+        game[1][1] = player;
         break;
       case "middleRight":
-        game[1][2] = "x";
+        game[1][2] = player;
         break;
       case "bottomLeft":
-        game[2][0] = "x";
+        game[2][0] = player;
         break;
       case "bottomMiddle":
-        game[2][1] = "x";
+        game[2][1] = player;
         break;
       case "bottomRight":
-        game[2][2] = "x";
+        game[2][2] = player;
         break;
     }
     displayBoard();
-    checkWin();
+    // Checks to see if there is a winner before recursive call
+    // TODO There needs to be a check for a washed game
+    if (checkWin() === false) {
+      askForMove();
+    }
   });
 };
 
+// Function that checks for a winner
 const checkWin = () => {
   if (game[0].every(i => i !== null && i === game[0][0])) {
     console.log(game[0][0] + "'s wins");
@@ -74,41 +85,48 @@ const checkWin = () => {
     return true;
   } else if (
     game[0][0] !== null &&
-    (game[0][0] === game[0][1] && game[0][1] === game[0][2])
+    game[0][0] === game[0][1] &&
+    game[0][1] === game[0][2]
   ) {
     console.log(game[0][0] + "'s wins");
     return true;
   } else if (
     game[1][0] !== null &&
-    (game[1][0] === game[1][0] && game[1][1] === game[1][2])
+    game[1][0] === game[1][0] &&
+    game[1][1] === game[1][2]
   ) {
     console.log(game[1][0] + "'s wins");
     return true;
   } else if (
     game[2][0] !== null &&
-    (game[2][0] === game[2][0] && game[2][1] === game[2][2])
+    game[2][0] === game[2][0] &&
+    game[2][1] === game[2][2]
   ) {
     console.log(game[2][0] + "'s wins");
     return true;
   } else if (
     game[0][0] !== null &&
-    (game[0][0] === game[1][1] && game[0][0] === game[2][2])
+    game[0][0] === game[1][1] &&
+    game[0][0] === game[2][2]
   ) {
     console.log(game[0][0] + "'s wins");
     return true;
   } else if (
     game[2][2] !== null &&
-    (game[2][2] === game[1][1] && game[2][2] === game[2][0])
+    game[2][2] === game[1][1] &&
+    game[2][2] === game[2][0]
   ) {
     console.log(game[0][0] + "'s wins");
     return true;
   } else return false;
 };
 
+// Fucntion that displays the game board
 const displayBoard = () => {
   console.log(game[0]);
   console.log(game[1]);
   console.log(game[2]);
 };
 
+// Runs the function that ask for a players move.
 askForMove();
