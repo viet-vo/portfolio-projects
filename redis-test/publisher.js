b29b9e2ee25b7c363496533934b3c2e8c633bea3
Viet Vo <viet.allen.qvo@gmail.com>
2020-06-15 20:04:59 -0700

    testing on react application

--- redis-test/publisher.js
+++ redis-test/publisher.js
@@ -1,15 +1,23 @@
 const redis = require("redis");
 const pub = redis.createClient();
 
-pub.on("error", function(error) {
+pub.on("error", function (error) {
   console.error(error);
 });
 
 pub.publish(
   "test" /* This is the channel */,
   "This is the test message 1" /* This is the message */,
-  function() {
+  function () {
     console.log("published w/ no issue");
     process.exit(0);
   }
 );
+
+// let message = document.getElementById("message");
+// const testPub = (message) => {
+//   pub.publish("test", message, function () {
+//     console.log("published w/ no issue");
+//     process.exit(0);
+//   })
+// }
